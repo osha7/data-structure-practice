a0424daa60b6faf75e0b98ac7ea80de891e03c13
osha7 <ogroetz@gmail.com>
2021-02-06 18:59:29 -0700

    hash table implemented in full

--- src/data-structures/hash-tables.js
+++ src/data-structures/hash-tables.js
@@ -2,29 +2,73 @@
 const hash = (key, size) => {
     let hashedKey = 0
     for (let i = 0; i < key.length; i++) {
-        hashedKey = key.charCodeAt(i)
+        hashedKey += key.charCodeAt(i)
     }
     return hashedKey % size
 }
 
-console.log(hash("hello", 100))
+class HashTable {
+    constructor() {
+        this.size = 25
+        this.buckets = Array(this.size)
 
-// ~/Development/algorithm-folder/data-structures (master ✘)✖✹✭ ᐅ node src/data-structures/hash-tables.js
-//=>  11
-// ~/Development/algorithm-folder/data-structures (master ✘)✖✹✭ ᐅ node src/data-structures/hash-tables.js
-//=>  11
-
-const hash = (key, size) => {
-    let hashedKey = 0
-    for (let i = 0; i < key.length; i++) {
-        hashedKey = key.charCodeAt(i)
+        for (let i = 0; i < this.buckets.length; i++) {
+            this.buckets[i] = new Map()
+        }
     }
-    return hashedKey % size
+
+    insert(key, value) {
+        let index = hash(key, this.size)
+        this.buckets[index].set(key, value)
+    }
+
+    search(key) {
+        let index = hash(key, this.size)
+        return this.buckets[index].get(key)
+    }
+
+    remove(key) {
+        let index = hash(key, this.size)
+        let deleted = this.buckets[index].get(key)
+        this.buckets[index].delete(key)
+        return deleted
+    }
 }
 
-console.log(hash("hello", 5))
+// const hashTable = new HashTable()
+
+// hashTable.insert('broccoli', 'green')
+// hashTable.insert('tomatoes', 'red')
+// hashTable.insert('squash', 'yellow')
+// hashTable.insert('kale', 'green')
+
+// hashTable
+
+// hashTable.search('tomatoes')
+
+// hashTable.remove('squash')
+// hashTable
+
+
+
 
 // ~/Development/algorithm-folder/data-structures (master ✘)✖✹✭ ᐅ node src/data-structures/hash-tables.js
-//=>  1
+//=>  2
 // ~/Development/algorithm-folder/data-structures (master ✘)✖✹✭ ᐅ node src/data-structures/hash-tables.js
-//=>  1
+//=>  2
+
+// const hash = (key, size) => {
+//     let hashedKey = 0
+//     for (let i = 0; i < key.length; i++) {
+//         hashedKey += key.charCodeAt(i)
+//     }
+//     return hashedKey % size
+// }
+
+// console.log(hash("hello", 100))
+
+// ~/Development/algorithm-folder/data-structures (master ✘)✖✹✭ ᐅ node src/data-structures/hash-tables.js
+//=>  32
+// ~/Development/algorithm-folder/data-structures (master ✘)✖✹✭ ᐅ node src/data-structures/hash-tables.js
+//=>  32
+
